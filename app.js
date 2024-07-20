b04fe796930cb5c7c518b9f1d01977d1f8031660
bibekv <whoisdinanath@gmail.com>
2024-07-21 01:07:17 +0545

    one more try 1111

--- app.js
+++ app.js
@@ -37,12 +37,15 @@ const allowedOrigins = [
 const corsOptions = {
   origin: function (origin, callback) {
     // Check if the incoming origin is in the allowedOrigins array
+    console.log('Origin:', origin);
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      console.log('Origin:', origin);
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
+
   methods: 'GET,HEAD,OPTIONS,POST,PUT',
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization',
 };
