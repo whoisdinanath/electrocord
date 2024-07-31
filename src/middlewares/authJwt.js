c255063e935395614f481dff44a28e52f759c28d
bibekv <whoisdinanath@gmail.com>
2024-07-31 20:27:28 +0545

    ......

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -7,10 +7,11 @@ dotenv.config();
 
 export const verifyToken = async (req, res, next) => {
     const { SECRET } = process.env;
+    console.log("req.cookies", req.signedCookies)
     try {
-        const token = req.cookies.token;
-        console.log("token", token)
-        if (!token) {
+        const {token} = req.signedCookies;
+        if (!token)
+		{
             return res.status(403).json(new ApiError(403, 'No token provided'));
         }
         const decoded = jwt.verify(token, SECRET);
